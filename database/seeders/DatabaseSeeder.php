1af59412766e02323becd2b7ce22295ae3586aa3
RyanArdyan <ryanardyan437@gmail.com>
2023-03-10 07:44:05 -0800

    Memodifikasi Database Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Pengaturan;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,9 +17,30 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        // tambah data di table user
+        // user pbarik buat
+        User::factory()->create([
+            // 1 adalah true berarti dia adalah admin, kalau 0 false berarti dia adalah kasir
+            'is_admin' => 1,
+            'gambar' => 'gambar_default.png',
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('pontianak')
+        ]);
+
+        // tambahkan 1 baris data ke table pengaturan menggunakan perintah php artisan db:seed
+        Pengaturan::create([
+            'pengaturan_id' => 1,
+            'nama_perusahaan' => 'Toko Mu',
+            'alamat_perusahaan' => 'Jl. Kibandang Samaran Ds. Slangit',
+            'telepon_perusahaan' => '0123456789',
+            // 1 berarti nota kecil
+            // 2 berarti nota besar 
+            'tipe_nota_perusahaan' => 1,
+            // default diskon untuk pelanggan adalah 5%
+            'diskon_perusahaan' => 5,
+            'logo_perusahaan' => 'logo_perusahaan.png',
+            'kartu_member' => 'kartu_member.png'
+        ]);
     }
 }
